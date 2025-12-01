c742b77d34e0e9d6fc9758fa9abc360ba2b1d75d
Kenji Sato <knjsa@example.com>
2025-12-01 12:42:02 -0300

    feat:context na pesquisa

--- cypress/e2e/gov_br/pesquisa.cy.js
+++ cypress/e2e/gov_br/pesquisa.cy.js
@@ -1,16 +1,18 @@
 /// <reference types="cypress" />
+context('GOV.BR Pesquisa', () => {
+  beforeEach(() => {
+    cy.visit('https://www.gov.br/pt-br')
+    cy.get('#barra-govbr').shadow().find('div.overlay-wrapper').click();
+  });
 it('Pesquisar sobre INSS', function() {
-  cy.visit('https://www.gov.br/pt-br')
-  cy.get('#barra-govbr').shadow().find('div.overlay-wrapper').click();
   cy.get('#searchtext-input').click();
   cy.get('#searchtext-input').type('INSS{enter}');
   
 });
 
 it('Pesquisar por Instagram', function() {
-  cy.visit('https://www.gov.br/pt-br')
-  cy.get('#barra-govbr').shadow().find('div.overlay-wrapper').click();
   cy.get('button.reject-all').click();
   cy.get('#portal-footer a[href="https://www.instagram.com/governodobrasil"]').click();
   
 });
+})
